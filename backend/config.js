723053e37ea8e8c3a257bf43dcc4ce981ff9e92c
ARNAV-PC <arnavrastogi47@gmail.com>
2026-03-09 14:44:47 +0530

    update

--- backend/config.js
+++ backend/config.js
@@ -1,16 +1,12 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import Stripe from "stripe";
 
-dotenv.config();  
+dotenv.config();
 
-const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
-
-const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
-
-const STRIPE_SECRET_KEY =  "sk_test_51T41VdQnuZRSHLNiDRmSKyfmxkVQctOh9hiZtb87xDO7o3BY0OY6K3pmmuTULM4irzpGvS46EZfxTT5JsgTW055300cbFEYHHa"
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default {
-    JWT_USER_PASSWORD,
-    JWT_ADMIN_PASSWORD,
-    STRIPE_SECRET_KEY,
+  JWT_USER_PASSWORD: process.env.JWT_USER_PASSWORD,
+  JWT_ADMIN_PASSWORD: process.env.JWT_ADMIN_PASSWORD,
+  STRIPE_SECRET_KEY: stripe
 };
-
