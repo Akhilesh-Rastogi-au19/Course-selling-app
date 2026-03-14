8fe2f38f9e4ffd795b2b296ad7e83783414ee643
ARNAV-PC <arnavrastogi47@gmail.com>
2026-03-14 14:26:28 +0530

    urla change

--- backend/index.js
+++ backend/index.js
@@ -22,26 +22,13 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-app.use(
-    cors({
-      origin: process.env.FRONTEND_URL,
-      credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-    })
-  );
-
-// app.use(
-//     cors({
-//       origin: [
-//         "http://localhost:5173",
-//         "https://courseaop.vercel.app"
-//       ],
-//       credentials: true,
-//       methods: ["GET", "POST", "PUT", "DELETE"],
-//       allowedHeaders: ["Content-Type", "Authorization"],
-//     })
-//   );
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
+  
 
 app.use(fileUpload({
     useTempFiles : true,
